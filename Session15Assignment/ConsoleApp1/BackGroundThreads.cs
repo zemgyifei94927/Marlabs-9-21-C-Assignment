d204187ceb0ed76e6d1736f04d00c2e2e5a25622
zemgyifei94927 <97491820+zemgyifei94927@users.noreply.github.com>
2023-10-02 13:20:02 -0400

    Add files via upload

---
+++ Session15Assignment/ConsoleApp1/BackGroundThreads.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    
+    internal class BackGroundThreads
+    {   
+        static void work()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Background thread running...");
+            }
+        }
+        //static void Main()
+        //{
+        //    var t = new Thread(work);
+        //    // Setting the thread to be background thread.
+        //    t.IsBackground = true;
+        //    t.Start();
+
+
+        //    for (int i = 0; i < 2; i++)
+        //    {
+        //        Thread.Sleep(200);
+        //        Console.WriteLine("Main thread running...");
+
+        //    }
+        //}
+    }
+}
+
---
+++ Session15Assignment/ConsoleApp1/ThreadPool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Demo
+{
+    internal class ThreadPoolDemo
+    {
+        static void ThreadMethod() { 
+            for (int i = 0; i < 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+
+        static void ThreadPoolMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Process));
+            }
+        }
+        static void Process(object callback)
+        {
+            Console.WriteLine("Working...");
+        }
+
+        //static void Main()
+        //{
+
+        //    Stopwatch sw = new Stopwatch();
+
+        //    Console.WriteLine("ThreadPool Started.");
+        //    sw.Start();
+        //    ThreadPoolMethod();
+        //    sw.Stop();
+
+        //    Thread.Sleep(500);
+
+        //    Console.WriteLine($"Time consumed by Thread pool is {sw.ElapsedTicks.ToString()}");
+
+        //    sw.Reset();
+
+        //    Console.WriteLine("Thread Started.");
+        //    sw.Start();
+        //    ThreadMethod();
+        //    sw.Stop();
+
+        //    Thread.Sleep(500);
+        //    Console.WriteLine($"Time consumed by Thread is {sw.ElapsedTicks.ToString()}");
+        //}
+    }
+}
