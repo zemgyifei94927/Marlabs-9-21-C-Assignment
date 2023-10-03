2026514fbb7c1c34e079728342390389eda87981
zemgyifei94927 <97491820+zemgyifei94927@users.noreply.github.com>
2023-10-03 13:22:08 -0400

    Add files via upload
    Assignment Accomplished

---
+++ Session16Assignment/ArraysRelated.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session16Assignment
+{
+    
+    internal class ArraysRelated
+    {
+        static void Main()
+        {   
+
+            
+            ArrayList myList = new ArrayList();
+            myList.Add(1);
+            myList.Add(2);
+
+            // Fixed size ArrayList
+            ArrayList fixedSizedList = ArrayList.FixedSize(myList);
+            try
+            {
+                fixedSizedList.Add(3);
+            }
+            catch(Exception e) {
+                Console.WriteLine("Displaying Exception: \n" + e.ToString()) ;
+            }
+            Console.WriteLine("press anykey to continue....");
+            Console.WriteLine("__________________________");
+            Console.ReadKey();
+
+            // AddRange
+            Console.WriteLine();
+            Console.WriteLine("Before AddRange: ");
+            foreach (int i in myList)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("After AddRange: ");
+            myList.AddRange(fixedSizedList);
+            foreach (int i in myList) {
+                Console.Write(i + " ");            
+            }
+            Console.WriteLine();
+            Console.WriteLine("press anykey to continue....");
+            Console.WriteLine("__________________________");
+            Console.ReadKey();
+
+            //Sort and BinarySearch
+            int[] myArray = new int[] { 17, 4, 3, 2, 6, 7, 12, 9, 10, 13, 8, 15, 1 };
+            Console.WriteLine();
+            Console.WriteLine("Current Array.");
+            foreach (int i in myArray) {
+                Console.Write(i + " ");
+            }
+            Array.Sort(myArray, 0, myArray.Length, StringComparer.CurrentCulture);
+            Console.WriteLine();
+            Console.WriteLine("Sorted Array.");
+            foreach (int i in myArray)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            int index = Array.BinarySearch(myArray, 0, myArray.Length, 5, StringComparer.CurrentCulture);
+            if (index < 0) { 
+                Console.WriteLine("Did not find the element!") ;
+            }
+            index = Array.BinarySearch(myArray, 0, myArray.Length, 17, StringComparer.CurrentCulture);
+            if (index >= 0)
+            {
+                Console.WriteLine($"Element is at index {index}");
+            }
+            Console.WriteLine("press anykey to continue....");
+            Console.WriteLine("__________________________");
+            Console.ReadKey();
+
+            // Shallow copy from clone
+            Car car1 = new Car("BoBoBo...");
+            Car car2 = new Car("DoomDoomDoom...");
+            Car car3 = new Car("VROOOOOOOOOM...");
+            Car[] cars = new Car[] { car1, car2 , car3};
+            Console.WriteLine();
+            Console.WriteLine("Initial cars' names:");
+            foreach (Car car in cars)
+            {
+                Console.WriteLine("Car name is "+ car.Name);
+            }
+            Car[] cars2 = (Car[])cars.Clone();
+            car1.Name = "AOAOAO...";
+            Console.WriteLine("Changed cars' names:");
+            foreach (Car car in cars2)
+            {
+                Console.WriteLine("Car name is " + car.Name);
+            }
+            Console.WriteLine("press anykey to continue....");
+            Console.WriteLine("__________________________");
+            Console.ReadKey();
+        }
+
+        class Car {
+            public string Name;
+            public Car(string Name) { 
+                this.Name = Name;
+            }
+        }
+    }
+}
---
+++ Session16Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Session16Assignment
+{
+    internal class InterThreadCommunication
+    {
+        static readonly object _lockObject = new object();
+        static void ThreadTwoWork() { 
+            Monitor.Enter(_lockObject);
+            for (int i = 0; i <= 20; i += 2) {
+                Console.WriteLine($"It is thread ###TWO### doing the job {i} right now.");
+                Monitor.Pulse(_lockObject);
+                if (i != 20) {
+                    Monitor.Wait(_lockObject);
+                }
+            }
+            Monitor.Exit(_lockObject);
+        }
+
+        static void ThreadOneWork()
+        {
+            Monitor.Enter(_lockObject);
+            for (int i = 1; i <= 20; i += 2)
+            {
+                Console.WriteLine($"It is thread $$$One$$$ doing the job {i} right now.");
+                Monitor.Pulse(_lockObject);
+                if (i != 19)
+                {
+                    Monitor.Wait(_lockObject);
+                }
+            }
+            Monitor.Exit(_lockObject);
+        }
+        //static void Main(string[] args)
+        //{
+        //    Thread ThreadTwo = new Thread(ThreadTwoWork);
+        //    Thread ThreadOne = new Thread(ThreadOneWork);
+
+        //    ThreadTwo.Start();
+        //    Thread.Sleep(500);
+        //    ThreadOne.Start();
+
+        //    ThreadTwo.Join();
+        //    ThreadOne.Join();
+
+        //    Console.ReadKey();
+
+        //}
+    }
+}
